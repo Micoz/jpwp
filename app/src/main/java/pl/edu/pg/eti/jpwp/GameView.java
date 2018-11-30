07d1369183ab274f9c4236d51a4ca6eb3966ebff
Micoz <micoz96@gmail.com>
2018-11-30 22:29:16 +0100

    Add game view and graphics

--- app/src/main/java/pl/edu/pg/eti/jpwp/GameView.java
+++ app/src/main/java/pl/edu/pg/eti/jpwp/GameView.java
@@ -1,12 +1,34 @@
 package pl.edu.pg.eti.jpwp;
 
 import android.content.Context;
-import android.support.annotation.MainThread;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.graphics.Paint;
+import android.graphics.Color;
+
+import static java.lang.StrictMath.round;
 
 class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
+    volatile boolean touched = false;
+    volatile float touched_x, touched_y;
+    private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    public static long averageFPS;
+    private Paint paint = new Paint();
+
+    private Bitmap manekin = Bitmap.createScaledBitmap(
+            BitmapFactory.decodeResource(getResources(),R.drawable.manekin),
+            (int)round(0.7*screenWidth), (int)round(0.9*screenHeight), true);
+    private Bitmap floor = Bitmap.createScaledBitmap(
+            BitmapFactory.decodeResource(getResources(),R.drawable.floor),
+            screenWidth, screenHeight, true);
 
     public GameView(Context context) {
         super(context);
@@ -20,14 +42,35 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        canvas.drawBitmap(floor, 0, 0, null);
+
+        canvas.drawBitmap(
+                manekin,
+                screenWidth/2 - manekin.getWidth()/2,
+                screenHeight - manekin.getHeight(),
+                null
+        );
+
+        canvas.drawText("FPS:" + Long.toString(averageFPS),20,50, paint);
+    }
+
+    @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(50);
+
         thread.setRunning(true);
         thread.start();
+
     }
 
     @Override
@@ -42,5 +85,34 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
             }
             retry = false;
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Auto-generated method stub
+
+        touched_x = event.getX();
+        touched_y = event.getY();
+
+        int action = event.getAction();
+        switch(action){
+            case MotionEvent.ACTION_DOWN:
+                touched = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touched = true;
+                break;
+            case MotionEvent.ACTION_UP:
+                touched = false;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                touched = false;
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+                touched = false;
+                break;
+            default:
+        }
+        return true; //processed
     }
 }
--- app/src/main/java/pl/edu/pg/eti/jpwp/MainThread.java
+++ app/src/main/java/pl/edu/pg/eti/jpwp/MainThread.java
@@ -5,6 +5,7 @@ import android.view.SurfaceHolder;
 import java.lang.Thread;
 
 public class MainThread extends Thread {
+    private int targetFPS = 60;
     private SurfaceHolder surfaceHolder;
     private GameView gameView;
     private boolean running;
@@ -21,17 +22,27 @@ public class MainThread extends Thread {
     }
 
     @Override
-    public void run() {
+    public void run(){
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long averageFPS;
+        long targetTime = 1000 / targetFPS;
+
         while (running) {
+            startTime = System.nanoTime();
             canvas = null;
 
             try {
                 canvas = this.surfaceHolder.lockCanvas();
-                synchronized(surfaceHolder) {
+                synchronized (surfaceHolder) {
                     this.gameView.update();
                     this.gameView.draw(canvas);
                 }
-            } catch (Exception e) {} finally {
+            } catch (Exception e) {
+            } finally {
                 if (canvas != null) {
                     try {
                         surfaceHolder.unlockCanvasAndPost(canvas);
@@ -39,6 +50,25 @@ public class MainThread extends Thread {
                         e.printStackTrace();
                     }
                 }
+            }
+
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - timeMillis;
+
+            try {
+                this.sleep(waitTime);
+            } catch (Exception e) {
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == targetFPS) {
+                averageFPS = 1000 / ((totalTime / frameCount) / 1000000);
+                GameView.averageFPS = averageFPS;
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println(averageFPS);
             }
         }
     }
